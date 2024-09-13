932a1f219d8388414127726178e28c9687952a2c
Evgeniy Buslovich <evgeniy_buslovich@epam.com>
2024-09-13 16:31:25 +0200

    Adding functions increment and decrement in script and description of them in .md

--- calculations.js
+++ calculations.js
@@ -27,7 +27,21 @@ function getRestAfterDivision(dividend, divisor) {
   }
 }
 
+function increment(number) {
+  number++;
+  return number;
+}
+
+function decrement(number) {
+  number--;
+  return number;
+}
+
+console.log(decrement(2));
+
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
+onsole.log(increment(2));
+onsole.log(decrement(2));
