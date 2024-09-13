ce0f84c495dfd9bcf07fc80a50761b806c8eb407
Evgeniy Buslovich <evgeniy_buslovich@epam.com>
2024-09-13 15:57:05 +0200

    HW_2.2.1_Fixing methods

--- calculations.js
+++ calculations.js
@@ -1,25 +1,33 @@
-function increaseBalance(x, y) {
-  x = x + y;
-  return x;
+function increaseBalance(balance, delta) {
+  balance = balance + delta;
+  return balance;
 }
 
-function decreaseBalance(x) {
-x = x - y;
-return x;
+function decreaseBalance(balance, delta) {
+  balance = balance - delta;
+  return balance;
 }
 
-function divideBalanceByAccounts(x, y) {
-  x = x / y;
-  return x;
-  console.log("x");
+function divideBalanceByAccounts(balance, numberOfAccounts) {
+  balance = balance / numberOfAccounts;
+  if (numberOfAccounts === 0) {
+    return "На ноль делить нельзя";
+  } else {
+    return balance;
+  }
 }
 
-function getRestAfterDivision(x, y) {
-  x = x % y;
-  return x;
+function getRestAfterDivision(dividend, divisor) {
+  dividend = dividend % divisor;
+
+  if (divisor === 0) {
+    return "На ноль делить нельзя";
+  } else {
+    return dividend;
+  }
 }
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
